308fa87403cd4b7e117da0dbda6a66940faf4431
tnqiang <tnqiang@163.com>
2017-08-07 15:16:34 +0800

    First commit

---
+++ Assets/Editor/GetAnimClipSize.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Profiling;
+using System.Reflection;
+using System;
+
+public class GetAnimClipSize 
+{
+    [MenuItem("Test/GetAllAnimSize")]
+    public static void GetAnimSize()
+    {
+        Assembly asm = Assembly.GetAssembly(typeof(Editor));
+        MethodInfo getAnimationClipStats = typeof(AnimationUtility).GetMethod("GetAnimationClipStats", BindingFlags.Static | BindingFlags.NonPublic);
+        Type aniclipstats = asm.GetType("UnityEditor.AnimationClipStats");
+        FieldInfo sizeInfo = aniclipstats.GetField("size", BindingFlags.Public | BindingFlags.Instance);
+        int allSize = 0;
+
+        string[] allAssetPaths = AssetDatabase.GetAllAssetPaths();
+        for (int k = 0; k < allAssetPaths.Length; ++k)
+        {
+            if (allAssetPaths[k].ToLower().EndsWith(".fbx"))
+            {
+                UnityEngine.Object[] objs = AssetDatabase.LoadAllAssetsAtPath(allAssetPaths[k]);
+                for (int i = 0; i < objs.Length; ++i)
+                {
+                    if (objs[i] is AnimationClip)
+                    {
+                        AnimationClip aniClip = objs[i] as AnimationClip;
+                        //Debug.Log(Profiler.GetRuntimeMemorySize(aniClip));//MemorySize
+
+                        if (objs[i].name.StartsWith("__preview__Take"))
+                        {
+                            var stats = getAnimationClipStats.Invoke(null, new object[]{ aniClip });
+                            allSize += (int)sizeInfo.GetValue(stats);
+                        }
+                        //var stats = getAnimationClipStats.Invoke(null, new object[]{ aniClip });
+                        //Debug.Log(objs[i].name + " " + EditorUtility.FormatBytes((int)sizeInfo.GetValue(stats)));//BlobSize
+                    }
+                }
+            }
+        }
+        Debug.Log("All Animation Size: " + EditorUtility.FormatBytes(allSize));
+    }
+
+    [MenuItem("Test/GetSelectedAnimSize")]
+    public static void GetSelectedAnimSize()
+    {
+        Assembly asm = Assembly.GetAssembly(typeof(Editor));
+        MethodInfo getAnimationClipStats = typeof(AnimationUtility).GetMethod("GetAnimationClipStats", BindingFlags.Static | BindingFlags.NonPublic);
+        Type aniclipstats = asm.GetType("UnityEditor.AnimationClipStats");
+        FieldInfo sizeInfo = aniclipstats.GetField ("size", BindingFlags.Public | BindingFlags.Instance);
+
+        if (Selection.assetGUIDs.Length > 0)
+        {
+            UnityEngine.Object[] objs = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]));
+            Debug.Log(AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]));
+            for (int i = 0; i < objs.Length; ++i)
+            {
+                if (objs[i] is AnimationClip)
+                {
+                    AnimationClip aniClip = objs[i] as AnimationClip;
+                    Debug.Log(Profiler.GetRuntimeMemorySize (aniClip));//MemorySize
+
+                    var stats = getAnimationClipStats.Invoke(null, new object[]{aniClip});
+                    Debug.Log(objs[i].name + " " + EditorUtility.FormatBytes((int)sizeInfo.GetValue(stats)));//BlobSize
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Editor/SetFbxImporterBatch.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class SetFbxImporterBatch : EditorWindow {
+
+    public enum AnimationCompressType
+    {
+        NONE,
+        KEY_FRAME_REDUCTION,
+        OPTIMAL
+    }
+
+    private float m_rotationErrorThreshold;
+    private float m_positionErrorThreshold;
+    private float m_scaleErrorThreshold;
+    private AnimationCompressType m_compressType;
+
+    [MenuItem("Tools/SetFbxImporterBatch")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        SetFbxImporterBatch window = (SetFbxImporterBatch)EditorWindow.GetWindow(typeof(SetFbxImporterBatch));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+
+        m_compressType = (AnimationCompressType)EditorGUILayout.EnumPopup(m_compressType);
+
+        if (m_compressType != AnimationCompressType.NONE)
+        {
+            m_rotationErrorThreshold = EditorGUILayout.FloatField("Rotation Error: ", m_rotationErrorThreshold);
+            m_positionErrorThreshold = EditorGUILayout.FloatField("Position Error: ", m_positionErrorThreshold);
+            m_scaleErrorThreshold = EditorGUILayout.FloatField("Scale Error: ", m_scaleErrorThreshold);
+        }
+
+        if (GUILayout.Button("Apply"))
+        {
+            string[] allAssetPaths = AssetDatabase.GetAllAssetPaths();
+            for (int i = 0; i < allAssetPaths.Length; ++i)
+            {
+                ModelImporter importer = ModelImporter.GetAtPath(allAssetPaths[i]) as ModelImporter;
+                if (importer != null)
+                {
+                    FormatModel(importer, m_compressType, m_rotationErrorThreshold, m_positionErrorThreshold, m_scaleErrorThreshold);
+                }
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Debug.Log(m_compressType);
+            Debug.Log(m_rotationErrorThreshold);
+            Debug.Log(m_positionErrorThreshold);
+            Debug.Log(m_scaleErrorThreshold);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void FormatModel(ModelImporter importer, AnimationCompressType type, float rotationErrorThreshold, float positionThreshold, float scaleThreshold)
+    {
+        if (type == AnimationCompressType.NONE)
+        {
+            importer.animationCompression = ModelImporterAnimationCompression.Off;
+        }
+        else if (type == AnimationCompressType.KEY_FRAME_REDUCTION)
+        {
+            importer.animationCompression = ModelImporterAnimationCompression.KeyframeReduction;
+            importer.animationRotationError = rotationErrorThreshold;
+            importer.animationPositionError = positionThreshold;
+            importer.animationScaleError = scaleThreshold;
+        }
+        else
+        {
+            importer.animationCompression = ModelImporterAnimationCompression.Optimal;
+            importer.animationRotationError = rotationErrorThreshold;
+            importer.animationPositionError = positionThreshold;
+            importer.animationScaleError = scaleThreshold;
+        }
+        importer.SaveAndReimport();
+    }
+}
